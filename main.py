10157fd3a759a0457405a23021a59bbdc6161007
johnm0bley <0hztone@gmail.com>
2022-01-17 06:18:42 +0900

    added chat tab

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    data = [
-        {'title': 'something',
-        'price': 'something',
-        'day': 'something'},
-
-        {'title': 'anything',
-        'price': 'anything',
-        'day': 'anything'}
+    intro1 = [
+        "俺はやることがあるから、あとから合流するわ。俺のパソコン使っていいから、昨日話したとおりにやっておいてほしい。",
+        "あ、余計なところは見なくていいのでwww",
+        "やっとあともう少しってところまで来たんだし、さっさと終わらせようぜ~",
+        'ユーザーネームは"miy@uch1"',
+        "パスワードはラミ・マレックの誕生日をYYYYMMDDにしてあるからよろしく。じゃあ。"
+    ]
+    intro2 = [
+        "たまには息を抜いてリラックスしろよな",
+        "河川敷で待ってるからさ",
+        "ごめんだけど、コーヒー買ってきてくれる？"
     ]
 
     return render_template(
         'base.html',
-        data = data
+        message1 = intro1,
+        message2 = intro2
     )
