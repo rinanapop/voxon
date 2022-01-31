d8640751527cbbdb1d28e88ac708b8c924557b28
rinanapop <rinanapop@gmail.com>
2022-01-31 12:31:29 +0900

    change whole scripts of switch-display.js, add skype's icon

--- static/js/switch-display.js
+++ static/js/switch-display.js
@@ -1,5 +1,25 @@
-var $target = document.querySelector('.target')
-var $button = document.querySelector('.mail-btn')
-$button.addEventListener('click', function() {
-    $target.classList.toggle('is-hidden')
+window.addEventListener("DOMContentLoaded", () => {
+  const switch_btn = document.getElementById("chat_btn");
+  const search_field = document.getElementById("chat_field");
+  
+  switch_btn.addEventListener("click", () => {
+    if(getComputedStyle(search_field).display == "none"){
+      search_field.style.display = "block";
+    } else {
+      search_field.style.display = "none";
+    }
+  })
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+  const switch_btn = document.getElementById("skypech_btn");
+  const search_field = document.getElementById("skypech_field");
+  
+  switch_btn.addEventListener("click", () => {
+    if(getComputedStyle(search_field).display == "none"){
+      search_field.style.display = "block";
+    } else {
+      search_field.style.display = "none";
+    }
+  })
 })
